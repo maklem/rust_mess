5116c397c28d2e05524eb6fad5b84f59b399945b
Dr. Marco Klement <marco-klement-git@leuchtstark.dev>
2025-09-29 11:46:19 +0200

    moves project to no_std

--- esp/src/main.rs
+++ esp/src/main.rs
@@ -1,10 +1,52 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
 
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+use esp_hal::clock::CpuClock;
+use esp_hal::main;
+use esp_hal::{
+    time::{Duration, Instant},
+    gpio::{Output, OutputConfig, Level},
+};
+use esp_hal::timer::timg::TimerGroup;
+use rtt_target::rprintln;
 
-    log::info!("Hello, world!");
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+extern crate alloc;
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[main]
+fn main() -> ! {
+    rtt_target::rtt_init_print!();
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let mut led_pin = Output::new(peripherals.GPIO4, Level::High, OutputConfig::default());
+
+    esp_alloc::heap_allocator!(size: 64 * 1024);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let _init = esp_wifi::init(timg0.timer0, esp_hal::rng::Rng::new(peripherals.RNG)).unwrap();
+
+    loop {
+        rprintln!("Hello world!");
+        let delay_start = Instant::now();
+        while delay_start.elapsed() < Duration::from_millis(500) {}
+        led_pin.set_high();
+        while delay_start.elapsed() < Duration::from_millis(1000) {}
+        led_pin.set_low();    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.0/examples/src/bin
 }
