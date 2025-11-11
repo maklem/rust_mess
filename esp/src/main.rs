2c92d8a5ee70354a005549e5840f414308f690d1
Dr. Marco Klement <marco-klement-git@leuchtstark.dev>
2025-11-11 15:27:37 +0100

    esp: structures and comments main function and loop

--- esp/src/main.rs
+++ esp/src/main.rs
@@ -15,8 +15,8 @@ use rtt_target::rprintln;
 use smoltcp::wire::{EthernetAddress, IpCidr};
 use tinymqtt::MqttClient;
 
-mod analog;
 use mess_lib::reset_on_failure_count::ResettingCounter;
+mod analog;
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -40,21 +40,24 @@ fn reset_esp() -> () {
 #[main]
 fn main() -> ! {
     rtt_target::rtt_init_print!();
+    esp_alloc::heap_allocator!(size: 64 * 1024);
+    let mut watchdog = ResettingCounter::new(reset_esp, 20);
 
+    /* initialize HAL */
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
-    let mut watchdog = ResettingCounter::new(reset_esp, 20);
-
+    /* initialize pins */
     let mut led_pin = Output::new(peripherals.GPIO4, Level::High, OutputConfig::default());
-
     let mut analog_pin = analog::SingleAnalogInput::new(peripherals.GPIO35, peripherals.ADC1);
 
-    esp_alloc::heap_allocator!(size: 64 * 1024);
+    /* initialize wifi hardware */
+    let hw_timer_group_0 = TimerGroup::new(peripherals.TIMG0);
+    let hw_rng = esp_hal::rng::Rng::new(peripherals.RNG);
+    let hw_wifi_controller = esp_wifi::init(hw_timer_group_0.timer0, hw_rng).unwrap();
+    let (mut wifi_controller, mut wifi_interfaces) = esp_wifi::wifi::new(&hw_wifi_controller, peripherals.WIFI).unwrap();
 
-    let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let wifi_init = esp_wifi::init(timg0.timer0, esp_hal::rng::Rng::new(peripherals.RNG)).unwrap();
-
+    /* connect wifi */
     let wifi_ssid = env!("WIFI_SSID").to_string();
     let wifi_pass = env!("WIFI_PASS").to_string();
     rprintln!("Connecting to '{ssid}' using '{pass}' ", ssid=wifi_ssid, pass=wifi_pass);
@@ -66,11 +69,7 @@ fn main() -> ! {
         channel: None,
         bssid: None,
     };
-
-    let (mut wifi_controller, mut wifi_interfaces) = esp_wifi::wifi::new(&wifi_init, peripherals.WIFI).unwrap();
-
     let _ = wifi_controller.start();
-
     let _ = wifi_controller.set_configuration(&esp_wifi::wifi::Configuration::Client(wifi_config));
     let status = wifi_controller.connect();
 
@@ -96,18 +95,7 @@ fn main() -> ! {
     }
     watchdog.reset();
    
-    let mut rx_data =  [0u8; 1024];
-    let rx_buffer = smoltcp::socket::tcp::SocketBuffer::new(&mut rx_data as &mut [u8]);
-    let mut tx_data =  [0u8; 1024];
-    let tx_buffer = smoltcp::socket::tcp::SocketBuffer::new(&mut tx_data as &mut [u8]);
-    let mut socket = smoltcp::socket::tcp::Socket::new(rx_buffer,tx_buffer);
-    socket.set_timeout(Option::Some(smoltcp::time::Duration::from_secs(600)));
-
-    rprintln!("connection state {:?}", socket.state());
-
-    let mut socket_data =  [smoltcp::iface::SocketStorage::EMPTY; 10];
-    let mut socket_set = smoltcp::iface::SocketSet::new(&mut socket_data as &mut [smoltcp::iface::SocketStorage]);
-
+   /* configure IPv4 for Wifi */
     let iface_config = smoltcp::iface::Config::new(
         smoltcp::wire::HardwareAddress::Ethernet(
             EthernetAddress(wifi_interfaces.sta.mac_address()).into()));
@@ -124,9 +112,18 @@ fn main() -> ! {
         .add_default_ipv4_route(smoltcp::wire::Ipv4Address::new(192, 168, 178, 1))
         .unwrap();
 
-    rprintln!("connection state {:?}", socket.state());
+    /* Set up TCP socket */
+    let mut rx_data =  [0u8; 1024];
+    let rx_buffer = smoltcp::socket::tcp::SocketBuffer::new(&mut rx_data as &mut [u8]);
+    let mut tx_data =  [0u8; 1024];
+    let tx_buffer = smoltcp::socket::tcp::SocketBuffer::new(&mut tx_data as &mut [u8]);
+    let mut socket = smoltcp::socket::tcp::Socket::new(rx_buffer,tx_buffer);
+    socket.set_timeout(Option::Some(smoltcp::time::Duration::from_secs(600)));
 
+    let mut socket_data =  [smoltcp::iface::SocketStorage::EMPTY; 10];
+    let mut socket_set = smoltcp::iface::SocketSet::new(&mut socket_data as &mut [smoltcp::iface::SocketStorage]);
     let tcp_handle = socket_set.add(socket);
+
     /* Connect TCP */
     {
         let connection = socket_set.get_mut::<smoltcp::socket::tcp::Socket>(tcp_handle);
@@ -182,15 +179,27 @@ fn main() -> ! {
         }
     }
 
+    /* main loop */
+    /* if something breaks, `watchdog` will be notified. It will reset the chip after N failures. */
     loop {
+        /* let wifi-hw communicate */
         let timestamp = now();
         iface.poll(timestamp, &mut wifi_interfaces.sta, &mut socket_set);
 
+        /* assert wifi is connected */
+        let wifi_status = wifi_controller.is_connected();
+        if wifi_status.is_err() || ! wifi_status.ok().unwrap() {
+            watchdog.increment_failure();
+        }
+
+        /* assert MQTT is connected */
         let connection = socket_set.get_mut::<smoltcp::socket::tcp::Socket>(tcp_handle);
 
         if connection.state() == smoltcp::socket::tcp::State::Closed {
             watchdog.increment_failure();
         }
+
+        /* on MQTT - process messages*/
         if connection.may_recv() && connection.can_recv() {
             let mut data = [0u8; 256];
             let state = connection.recv_slice( &mut data as &mut [u8]);
@@ -208,6 +217,7 @@ fn main() -> ! {
             }
         }
 
+        /* measure value */
         let pin_reading = analog_pin.read();
         let pin_value = if pin_reading.is_ok() {
             pin_reading.ok().unwrap()
@@ -216,6 +226,7 @@ fn main() -> ! {
             0.0
         };
 
+        /* on MQTT - send measured value */
         if client.is_connected() {
             let payload_str = format!("{:.6e}", pin_value);
             let payload = payload_str.as_bytes();
@@ -234,12 +245,11 @@ fn main() -> ! {
 
         rprintln!("Measured: {:5.2e}      -- Wifi: {:?} -- TCP: {:?}", pin_value, wifi_controller.is_connected(), connection.state());
         
+        /* blink LEDs ... and delay iterations */
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
         led_pin.set_high();
         while delay_start.elapsed() < Duration::from_millis(1000) {}
         led_pin.set_low();
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.0/examples/src/bin
 }
